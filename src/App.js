102346969fdb60a9dc7b65d4e0f620cf220a3707
zoom2009 <afumagic3@gmail.com>
2020-03-19 01:44:10 +0700

    init project & page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import { Layout, Menu, Breadcrumb } from 'antd'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const { Header, Content, Footer } = Layout
+
+const Item = styled(Menu.Item)`
+  height: 100%;
+  padding-top: 8px;
+  font-size: 18px;
+`
+
+const Logo = () => (
+  <div style={{ float: 'left', color: 'white', paddingRight: '50px' }}>LOGO</div>
+)
+
+const ContentSection = styled.div`
+  height: calc(100vh - 190px);
+`
+
+const Tab = ({ tabs }) => (
+  <Breadcrumb style={{ margin: '16px 0' }}>
+    {tabs.map(({ path, text }) => (
+      <Breadcrumb.Item key={path}><Link to={path}>{text}</Link></Breadcrumb.Item>
+    ))}
+  </Breadcrumb>
+)
+
+const CreatePage = () => (
+  <div>
+    <Tab tabs={[
+      { path: '/home/create', text: 'Create Data' },
+      { path: '/home/list', text: 'List Data' }
+    ]} />
+    <ContentSection>
+      <Switch>
+        <Route exact path={['/', '/home', '/home/create']}>
+          <div>Create Form</div>
+        </Route>
+        <Route path="/home/list">
+          <div>List Data</div>
+        </Route>
+      </Switch>
+    </ContentSection>
+  </div>
+)
+
+const AboutPage = () => (
+  <div>
+    <Tab tabs={[
+      { path: '/about/experience', text: 'My Experience' },
+      { path: '/about/skill', text: 'My Skill' }
+    ]} />
+    <ContentSection>
+      <Switch>
+        <Route exact path={['/', '/about', '/about/experience']}>
+          <div>My Experience</div>
+        </Route>
+        <Route path="/about/skill">
+          <div>My Skill</div>
+        </Route>
+      </Switch>
+    </ContentSection>
+  </div>
+)
+
+const App = () => (
+  <BrowserRouter>
+    <Layout className="layout">
+      <Header>
+        <Logo />
+        <Menu
+          style={{ height: '100%' }}
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+          <Item key="1"><Link to="/home">Home</Link></Item>
+          <Item key="2"><Link to="/about">About me</Link></Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Switch>
+          <Route path="/about" component={AboutPage} />
+          <Route path={['/', '/home']} component={CreatePage} />
+        </Switch>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </BrowserRouter>
+)
 
-export default App;
+export default App
